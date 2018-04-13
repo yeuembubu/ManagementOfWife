612299ce6e0958d295024eaf0189d18530705885
huy_dq <huy_dg@septeni-technology.jp>
2018-04-13 18:34:07 +0700

    Update default currency dialog, statement date, due date dialog.

--- app/src/main/java/vn/com/hn/management/wife/huy_dq/managementofwife/MainActivity.java
+++ app/src/main/java/vn/com/hn/management/wife/huy_dq/managementofwife/MainActivity.java
@@ -1,11 +1,15 @@
 package vn.com.hn.management.wife.huy_dq.managementofwife;
 
-import android.arch.persistence.room.Room;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,10 +20,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
@@ -59,6 +67,9 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+        setDefaultCurrency();
+        setDefaultStatementDateAndDueDate();
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -67,6 +78,76 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void setDefaultStatementDateAndDueDate() {
+        SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        String statementDate = prefs.getString("Statement", null);
+        String dueDate = prefs.getString("Due", null);
+        if (statementDate == null || dueDate == null) {
+            int time = 0;
+            while (showStatementDateAndDueDateDialog(time) < 2) {
+                Log.i(">>>>>>>>>>>>>>>>>>>>>", "OK");
+            }
+        }
+    }
+
+    public static final String MY_PREFS_NAME = "MyPrefsFile";
+
+    private void setDefaultCurrency() {
+        SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        String restoredText = prefs.getString("Currency", null);
+        if (restoredText == null) {
+            showCurrencyDialog();
+        }
+    }
+
+    private void showCurrencyDialog() {
+        final CharSequence[] charSequence = new CharSequence[]{"USD", "VND"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Default Currency")
+                .setSingleChoiceItems(charSequence, 0, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+                        editor.putString("Currency", charSequence[which].toString());
+                        editor.apply();
+                    }
+                })
+                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        builder.create().show();
+    }
+
+    private EditText inputDate;
+
+    private int showStatementDateAndDueDateDialog(int time) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Input your statement date & due date");
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.date_dialog, null);
+        builder.setView(dialogView);
+
+        inputDate = dialogView.findViewById(R.id.etDate);
+
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                Toast.makeText(getApplicationContext(), inputDate.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                dialog.dismiss();
+            }
+        });
+        builder.create().show();
+        return time++;
     }
 
     private List<String> getPlanDataList() {
